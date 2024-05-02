472d95fe1a933e191222eed2dc825db6462a51da
ianhara <ianhara97@gmail.com>
2024-05-02 12:38:04 -0400

    finished tag route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -37,4 +37,50 @@ router.get('/:id', async (req, res) => {
 router.post('/', async (req, res) => {
   try {
     const newTag = await Tag.create(req.body);
-    res.status(201).json
+    res.status(201).json(newTag);
+  } catch (err) {
+    console.error(err);
+    res.status(400).json(err);
+  }
+});
+
+// PUT update a tag's name by its `id` value
+router.put('/:id', async (req, res) => {
+  try {
+    const updatedTag = await Tag.update(req.body, {
+      where: {
+        id: req.params.id
+      }
+    });
+    if (!updatedTag[0]) {
+      res.status(404).json({ message: 'Tag not found' });
+      return;
+    }
+    res.json(updatedTag);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json(err);
+  }
+});
+
+// DELETE a tag by its `id` value
+router.delete('/:id', async (req, res) => {
+  try {
+    const deletedTag = await Tag.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    if (!deletedTag) {
+      res.status(404).json({ message: 'Tag not found' });
+      return;
+    }
+    res.json(deletedTag);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
+
